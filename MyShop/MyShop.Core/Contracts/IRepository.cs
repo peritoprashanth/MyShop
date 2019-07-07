76f3ce7558e7e8fc3b6b7a3c1cef59abf13d1e2c
PeritoPrashanth <peritonaga@gmail.com>
2019-07-07 09:34:39 -0500

    Dependency Injection

---
+++ MyShop/MyShop.Core/Contracts/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Contracts
+{
+   public  interface IRepository<T>
+    {
+        void Commit();
+        void Insert(T t);
+        void Update(T t);
+        T Find(string Id);
+
+        //public T FindByName(string name)
+        //{
+        //    T product = items.Find(p => p.Name == name);
+        //    if (product != null)
+        //    {
+        //        return product;
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Product not found");
+        //    }
+        //}
+
+        IQueryable<T> Collection();
+        void Delete(string Id);
+        
+    }
+}
